98bf9aa6e3b9f58ccd6fb79bbc091882c2ef5b55
sam-PC\sam <sam@sapido.com.tw>
2018-02-26 15:59:57 +0800

    change_that

--- index.py
+++ index.py
@@ -7,4 +7,24 @@ class One():
     def get_age(self):
         return self.age
 
+
 class two():
+if __name__ =='__main__':    def __init__(self):
+        self.name = 'two'
+
+
+class three():
+    def __init__(self, name, descript):
+        self.name = name
+        self.descript = descript
+
+class four():
+    def __init__(self, name, tagname):
+        self.name = name
+        self.tagname = tagname
+
+if __name__ == '__main__':
+    two = two()
+    three = three('tina', 'that my helf.')
+    four = four('four_one', 'four_two')
+    print(three.name)
