133a11d3c1fd16750224c25c2c3f4019e3666118
sam-PC\sam <sam@sapido.com.tw>
2018-02-26 13:31:06 +0800

    first

---
+++ index.py
@@ -0,0 +1,12 @@
+class One():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def get_name(self):
+        return self.name
+    def get_age(self):
+        return self.age
+
+if __name__ =='__main__':
+    one = One('sam', 45)
+    print(one.get_name())
